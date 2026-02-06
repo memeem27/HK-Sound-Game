423eab2800fd2ba74800dff6295e3070679f0ace
memeem27 <simonchristensenalt@outlook.com>
2026-02-05 20:06:41 -0800

    More firebase lol

--- firebase.js
+++ firebase.js
@@ -1,8 +1,6 @@
-// Firebase core
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
 
-// Your Firebase config
 const firebaseConfig = {
     apiKey: "AIzaSyCcP2iVzSg2cgW7dn_3JRWIyxsuhq7aKug",
     authDomain: "hollow-knight-sound-game.firebaseapp.com",
@@ -13,8 +11,5 @@ const firebaseConfig = {
     measurementId: "G-M3ZT0KJLZV"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firestore
 export const db = getFirestore(app);
